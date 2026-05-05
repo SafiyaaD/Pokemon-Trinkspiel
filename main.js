113ac16dbfe932a43ec56f0bd98dad17cb54e85d
SafiyaaD <griese96@web.de>
2026-05-05 21:03:34 +0200

    2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js
 import { loadHotspots, renderHotspots } from './game/board.js';
-import { setupUI } from './ui/gameflow.js';
+import { setupUI } from './ui/gameflow2.js';
 
 async function init() {
   const hotspots = await loadHotspots();
